99acea23217c8d287ccb92b5015364ca5beff3b9
Vangelis Tsoumenis <kioopi@gmail.com>
2011-05-30 01:10:15 +0200

    added auto cropping of image when saved.

--- webroot/js/script.js
+++ webroot/js/script.js
@@ -1,6 +1,95 @@
 var IMAGEPROXY_PREFIX = '/img/'; 
 
-// add a polyfill for bind 
+/*
+function setPixel(imageData, x, y, r, g, b, a) {
+  var index = (x + y * imageData.width) * 4;
+  var data = imageData.data; 
+  data[index+0] = r;
+  data[index+1] = g;
+  data[index+2] = b;
+  data[index+3] = a;
+}
+*/ 
+ 
+
+function pixelAt(imageData,x,y){
+  var data = imageData.data; 
+  var index = (x + y * imageData.width) * 4;
+  return {
+    r: data[index+0], 
+    g: data[index+1], 
+    b: data[index+2], 
+    a: data[index+3] 
+  }; 
+} 
+
+function findEdges(imageData) { 
+  var w = imageData.width,
+      h = imageData.height,
+      x,y, // cursor
+      topleft = {x:undefined, y:undefined}, 
+      bottomright = {x:undefined, y:undefined}; 
+
+  // aproach from top 
+  for (y=0; y<h; y++) { 
+    for (x=0; x<w; x++) { 
+      if (pixelAt(imageData,x,y).a > 0) {
+        topleft.y = y;	
+	break; 
+      } 
+    } 
+    if (topleft.y) break; 
+  } 
+
+  // from left    
+  for (x=0; x<w; x++) { 
+    for (y=0; y<h; y++) { 
+      if (pixelAt(imageData,x,y).a > 0) {
+        topleft.x = x; 
+	break;
+      } 
+    } 
+    if (topleft.x) break; 
+  } 
+
+  // from right
+  for (x=w; x>=0; x--) { 
+    for (y=0; y<h; y++) { 
+      if (pixelAt(imageData,x,y).a > 0) {
+        bottomright.x = x;
+	break;
+      } 
+    } 
+    if (bottomright.x) break; 
+  } 
+
+  // from bottom
+  for (y=h; y>=0; y--) { 
+    for (x=0; x<w; x++) { 
+      if (pixelAt(imageData,x,y).a > 0) {
+        bottomright.y = y;
+	break;
+      } 
+    } 
+    if (bottomright.y) break; 
+  } 
+  
+  console.log(topleft, bottomright);
+  if (!(topleft.x && topleft.y && bottomright.x && bottomright.y)) { 
+    console.error('No boundaries found!');
+    return null;
+  } else { 
+    return { 
+      x: topleft.x,
+      y: topleft.y, 
+      w: bottomright.x - topleft.x,
+      h: bottomright.y - topleft.y
+    } 
+  } 
+} 
+
+// add a polyfill for bind from mdc
+// is this necc? used only once. 
 if ( !Function.prototype.bind ) {
   Function.prototype.bind = function( obj ) {
     var slice = [].slice,
@@ -35,13 +124,10 @@ window.addEventListener("load", function(e){
   }, false);
 
   byId('helpBtn').addEventListener('click', function(e){ 
-    console.log('hello');
     e.stopPropagation();
     if(byId('helpbox').classList.toggle('active')){ 
-            
       var closeHelpBox = function(e){ 
         byId('helpbox').classList.remove('active'); 
-
 	document.removeEventListener('click', closeHelpBox, false); 
       } 
       document.addEventListener('click', closeHelpBox, false); 
@@ -49,7 +135,7 @@ window.addEventListener("load", function(e){
 
   }, false); 
 
-  var canvas = new fabric.Element('canvas');
+  canvas = new fabric.Element('canvas');
   var originalImage = null;
    billyfaces = []; 
 
@@ -73,7 +159,7 @@ window.addEventListener("load", function(e){
 
 
   byId('loadOriginalBtn').addEventListener('click', function(){ 
-        loadOriginal();
+    loadOriginal();
   }, false); 
 
 
@@ -108,7 +194,31 @@ window.addEventListener("load", function(e){
 
   byId('saveBtn').addEventListener('click', function(){ 
     canvas.deactivateAll();
-    window.open(canvas.toDataURL('png')); 
+    if (!canvas.isEmpty()) { 
+      var imgdata = canvas.toDataURL('png'); 
+    
+      var ce = document.getElementsByClassName('lower-canvas')[0],
+          ct = ce.getContext("2d"),
+          id = ct.getImageData(0,0, ce.width, ce.height),
+          rect = findEdges(id); // find boundaries
+	 // ct.putImageData(id,0,0); 
+
+      if(rect){ 
+        // create new canvas 
+        var tmpCan = document.createElement('canvas'); 
+	tmpCan.setAttribute('width', rect.w);
+	tmpCan.setAttribute('height', rect.h);
+	var ctx = tmpCan.getContext('2d');
+        // copy cropped data to new canvas
+        var croppedData = ct.getImageData(rect.x, rect.y, rect.w, rect.h); 
+	ctx.putImageData(croppedData, 0,0); 
+        // get imgdata from new canvas
+	imgdata = tmpCan.toDataURL('png'); 
+        window.open(imgdata); 
+      }
+      window.open(imgdata); 
+
+    }  
   }, false); 
 
 
@@ -125,7 +235,7 @@ window.addEventListener("load", function(e){
   dropzone.addEventListener("drop", function(e){ 
     noop(e); 
 
-    if(e.dataTransfer){ 
+    if (e.dataTransfer) { 
       var files = e.dataTransfer.files; 
       if(typeof files == "undefined" || files.length == 0){ 
         console.log('No files dropped!?');  
@@ -163,6 +273,4 @@ window.addEventListener("load", function(e){
       reader.readAsDataURL(files[0]); // we only care about one dropped image. 
     }
   }, false); 
-
 }, false); 
-
